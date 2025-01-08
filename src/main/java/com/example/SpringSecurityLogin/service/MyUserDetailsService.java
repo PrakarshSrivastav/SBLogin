db8e96789dd05f756d046631f869620b61246820
PrakarshSrivastav <prafir10@gmail.com>
2025-01-08 01:09:48 +0100

    Created Service, Repo and connected to the database using JPA,*->DAO

---
+++ src/main/java/com/example/SpringSecurityLogin/service/MyUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.example.SpringSecurityLogin.service;
+
+import com.example.SpringSecurityLogin.model.UserPrincipal;
+import com.example.SpringSecurityLogin.model.Users;
+import com.example.SpringSecurityLogin.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = userRepo.findByUsername(username);
+        if (user == null) {
+            System.out.println("User Not Found");
+            throw new UsernameNotFoundException("user not found");
+        }
+
+        return new UserPrincipal(user);
+    }
+}
